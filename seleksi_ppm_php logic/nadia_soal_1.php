7fec5616bfcb3625395fbc95556d083a70ca3747
Nadia Maharani <75023363+nadiamaharani25@users.noreply.github.com>
2022-04-06 16:28:02 +0700

    Update

--- seleksi_ppm_php logic/nadia_soal_1.php
+++ seleksi_ppm_php logic/nadia_soal_1.php
@@ -14,7 +14,7 @@
  		echo "Total 1 lusin : 12 x Rp 1.750   = " .rupiah($total). "<br>";
 
  		$subtotal = 5 * $uang; //Uang Rian 5 lembar 5000 = 25000
- 		echo "Uang Rian : 5 lembar * Rp 5.000 = " .rupiah($subtotal). "<br><br>";
+ 		echo "Uang Rian adalah : 5 lembar * Rp 5.000 = " .rupiah($subtotal). "<br><br>";
 
  		$kembalian = $subtotal - $total;
  		echo "Jadi, kembaliannya adalah   : <br> Rp 25.000 - Rp 21.000 = " .rupiah($kembalian);
